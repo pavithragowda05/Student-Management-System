9f8ca36d1a47ae8a17881953ed7be4350c25e9d3
pavithragowda05 <pavithragowda954@gmail.com>
2026-07-06 23:00:31 +0530

    Created student database schema

--- App.py
+++ App.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template , request
 
 app = Flask(__name__)
+import mysql.connector
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="anku123",
+    database="StudentDB"
+)
+cursor = db.cursor()
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -23,10 +31,20 @@ def save():
     usn = request.form["usn"]
     name = request.form["name"]
     year = request.form["year"]
+    cgpa =request.form["cgpa"]
     # Write the student data to a file or database
-    with open("studentdata.txt", "a") as file:
-        file.write(f"{usn},{name},{year}\n")
-    return render_template("index.html")
+    # with open("studentdata.txt", "a") as file:
+    #     file.write(f"{usn},{name},{year}\n")
+    # return render_template("index.html")
+
+    #writing data into database
+
+    sql="INSERT INTO Student(usn ,name ,year ,cgpa) VALUES(%s ,%s ,%s , %s)"
+    values=(usn ,name ,year ,cgpa)
+    cursor.execute(sql,values)
+    db.commit()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
